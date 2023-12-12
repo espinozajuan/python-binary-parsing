6fc1eceeab5d588a6bcc0e8e94d53e9962a08c10
Juan Gabriel Espinoza <136479329+juanespinozaa@users.noreply.github.com>
2023-12-12 02:16:33 -0300

    parse XML when loading and serialize XML when saving using pytinyxml2

--- panflute_test.py
+++ panflute_test.py
@@ -6,7 +6,8 @@ from typeguard import typechecked
 class FXP:
     @typechecked
     def __init__(self, version: int, fxId: int, fxVersion: int, numPrograms: int, 
-                 prgName: str, chunkSize: int, xmlContent: ByteString, wavetables: List[ByteString]):
+                 prgName: str, chunkSize: int, fxp_header: ByteString, non_xml_data_before: ByteString, 
+                 xmlContent: ByteString, non_xml_data_after: ByteString, wavetables: List[ByteString]):
         assert len(prgName.encode('utf-8')) <= 28, "Program name must be at most 28 bytes long"
         
         self.version: int = version
@@ -15,69 +16,62 @@ class FXP:
         self.numPrograms: int = numPrograms
         self.prgName: str = prgName
         self.chunkSize: int = chunkSize
+        self.fxp_header: ByteString = fxp_header
+        self.non_xml_data_before: ByteString = non_xml_data_before
         self.xmlContent: ByteString = xmlContent
+        self.non_xml_data_after: ByteString = non_xml_data_after
         self.wavetables: List[ByteString] = wavetables
 
     @typechecked
     def save(self, filename: str) -> None:
-        fxp_header: ByteString = struct.pack(
-            ">4si4siiii28si",
-            b'CcnK',
-            0,
-            b'FPCh',
-            self.version,
-            self.fxId,
-            self.fxVersion,
-            self.numPrograms,
-            self.prgName.encode('utf-8'),
-            len(self.xmlContent)
-        )
-
-        assert len(fxp_header) == 60, "FXP header size must be 60 bytes"
-
-        wavetable_data: ByteString = b''.join(self.wavetables)
-
         with open(filename, 'wb') as f:
-            f.write(fxp_header)
+            f.write(self.fxp_header)
+            f.write(self.non_xml_data_before)
             f.write(self.xmlContent)
-            f.write(wavetable_data)
+            f.write(self.non_xml_data_after)
+            for wt in self.wavetables:
+                f.write(wt)  # Write wavetable data
 
     @staticmethod
     @typechecked
     def load(filename: str) -> "FXP":
         with open(filename, 'rb') as f:
+            # Read the FXP header
             fxp_header: ByteString = f.read(60)
             assert len(fxp_header) == 60, "FXP header size must be 60 bytes"
 
-            chunkmagic, byteSize, fxMagic, version, fxId, fxVersion, numPrograms, prgName, chunkSize = struct.unpack(
-                ">4si4siiii28si", fxp_header)
+            # Read the entire content after the header
+            content = f.read()
 
-            xml_content: ByteString = f.read(chunkSize)
-            wavetables: ByteString = f.read()
+            # Find the start and end of the XML part
+            start = content.find(b"<?xml")
+            end = content.find(b"</patch>") + len(b"</patch>")
 
-            assert len(prgName.strip(b'\x00')) <= 28, "Program name must be at most 28 bytes long"
+            # Separate the file into parts
+            non_xml_data_before = content[:start] if start != -1 else b''
+            xml_content = content[start:end] if start != -1 and end != -1 else b''
+            non_xml_data_after = content[end:] if end != -1 else b''
 
-        return FXP(version, fxId, fxVersion, numPrograms, prgName.strip(b'\x00').decode('utf-8'), 
-                   chunkSize, xml_content, [wavetables])
+            # Dummy values for version, fxId, etc. (update as needed)
+            return FXP(0, 0, 0, 0, '', 0, fxp_header, non_xml_data_before, xml_content, non_xml_data_after, [])
 
 if __name__ == "__main__":
     # Load the original FXP file
-    original_fxp = FXP.load(r"C:\Users\Juan\Desktop\Boom.fxp")
+    original_fxp = FXP.load(r"C:\Users\Juan\Desktop\Siren.fxp")
 
     # Save the loaded FXP to a new file
-    original_fxp.save(r"C:\Users\Juan\Desktop\new_Boom.fxp")
+    original_fxp.save(r"C:\Users\Juan\Desktop\new_Siren.fxp")
 
     # Read the contents of the original and new FXP files
-    original_fxp_data = open(r"C:\Users\Juan\Desktop\Boom.fxp", "rb").read()
-    new_fxp_data = open(r"C:\Users\Juan\Desktop\new_Boom.fxp", "rb").read()
+    original_fxp_data = open(r"C:\Users\Juan\Desktop\Siren.fxp", "rb").read()
+    new_fxp_data = open(r"C:\Users\Juan\Desktop\new_Siren.fxp", "rb").read()
 
     # Export the contents to .txt files for comparison
-    with open(r"C:\Users\Juan\Desktop\original_Boom.txt", "wb") as file:
+    with open(r"C:\Users\Juan\Desktop\original_Siren.txt", "wb") as file:
         file.write(original_fxp_data)
 
-    with open(r"C:\Users\Juan\Desktop\new_Boom.txt", "wb") as file:
+    with open(r"C:\Users\Juan\Desktop\new_Siren.txt", "wb") as file:
         file.write(new_fxp_data)
 
     # Assert that the contents are the same
     assert original_fxp_data == new_fxp_data, "The original and new FXP files are not identical"
-
